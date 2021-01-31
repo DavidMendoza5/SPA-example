88e439aec5d64fee1f491cc0afbc9524b96006ee
David <cruz-azul99@hotmail.com>
2021-01-31 10:24:00 -0600

    Vue router added with its configuration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+
+// Creamos un enrutador
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const enrutador = new VueRouter({
+  routes: routes
+})
+
+
 new Vue({
+  // Indicamos quién es el enrutador
+  router: enrutador,
   render: h => h(App),
 }).$mount('#app')
