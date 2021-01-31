6882095362e6ccab84736b69811d2ebbe2f33ac8
David <cruz-azul99@hotmail.com>
2021-01-31 11:24:21 -0600

    router link tag modified

--- src/components/Encabezado.vue
+++ src/components/Encabezado.vue
@@ -5,8 +5,8 @@
                 <!-- El router link permite que el enrutador se encargue de las rutas -->
                 <!-- El atributo tag nos permite indicar sobre qué tag se encuentra el router-link sin necesidad de ponerlo -->
                 <!-- El atributo active class nos permite darle clases a nuestro router link y de esta forma usar css -->
-                <router-link tag="li" to="/" active-class="active" exact="/"><a>Inicio</a></router-link>
-                <router-link tag="li" to="/usuarios" active-class="active" exact="/usuarios"><a>Usuarios</a></router-link>
+                <router-link tag="li" to="/" active-class="active" exact><a>Inicio</a></router-link>
+                <router-link tag="li" to="/usuarios" active-class="active" exact><a>Usuarios</a></router-link>
             </ul>
             <h3 class="text muted">Aplicación usuarios</h3>
         </nav>
