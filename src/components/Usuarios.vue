504b440747830393d2aeee582d19fcfd0eed08e2
David <cruz-azul99@hotmail.com>
2021-01-31 11:11:05 -0600

    Component Usuarios created - This component list all the users that we have on our API, in this case a jsonplaceholder API is used

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Lista de usuarios</p>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="usuario in usuarios" :key="usuario.id">
+                {{ usuario.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            usuarios: {}
+        }
+    },
+    created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/users').then(respuesta => respuesta.json()).then(usuarios => this.usuarios = usuarios)
+    }
+}
+</script>
