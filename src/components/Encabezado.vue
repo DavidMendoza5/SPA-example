6ec4d017118e22e5fde37bbe5584669e7782ca50
David <cruz-azul99@hotmail.com>
2021-01-31 11:08:19 -0600

    Router link added on the nav bar

--- src/components/Encabezado.vue
+++ src/components/Encabezado.vue
@@ -2,8 +2,11 @@
     <div class="header clearfix">
         <nav>
             <ul class="nav nav-pills pull-right">
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Usuarios</a></li>
+                <!-- El router link permite que el enrutador se encargue de las rutas -->
+                <!-- El atributo tag nos permite indicar sobre qué tag se encuentra el router-link sin necesidad de ponerlo -->
+                <!-- El atributo active class nos permite darle clases a nuestro router link y de esta forma usar css -->
+                <router-link tag="li" to="/" active-class="active" exact="/"><a>Inicio</a></router-link>
+                <router-link tag="li" to="/usuarios" active-class="active" exact="/usuarios"><a>Usuarios</a></router-link>
             </ul>
             <h3 class="text muted">Aplicación usuarios</h3>
         </nav>
