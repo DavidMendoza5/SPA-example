7043287c82173cd075bcb3fd734a1d2a48c2fad6
David <cruz-azul99@hotmail.com>
2021-01-31 11:09:12 -0600

    Vue resource added to the project and mode history added to the vue router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import { routes } from './routes'
 
 // Creamos un enrutador
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
 const enrutador = new VueRouter({
-  routes: routes
+  routes: routes,
+  // Srive para quitar el # de la url, si se va a correr en producción, se deben cambiar las configuraciones del servidor, en la página de Vue están
+  mode: 'history'
 })
 
 
