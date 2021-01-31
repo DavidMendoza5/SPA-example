80031f2fe87af1c7a774505da6e52460d0025fd5
David <cruz-azul99@hotmail.com>
2021-01-31 11:09:46 -0600

    Users route added

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 // import { component } from "vue/types/umd"
 import InicioComponent from './components/inicioComponent.vue'
+import UsuariosComponent from './components/Usuarios.vue'
 
 // Las rutas son un arreglo de objetos
 export const routes = [
     {
         path: '',
         component: InicioComponent
+    },
+    {
+        path: '/usuarios',
+        component: UsuariosComponent
     }
 ]
