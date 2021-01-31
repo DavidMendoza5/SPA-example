0d5e685cf23e096c69cb820d8565d95db0b50e89
David <cruz-azul99@hotmail.com>
2021-01-31 10:24:50 -0600

    A file that will contain the routes was added

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+// import { component } from "vue/types/umd"
+import InicioComponent from './components/inicioComponent.vue'
+
+// Las rutas son un arreglo de objetos
+export const routes = [
+    {
+        path: '',
+        component: InicioComponent
+    }
+]
