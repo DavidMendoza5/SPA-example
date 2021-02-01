33c301d9b5d9f1409bed6163166c9e3be342ef22
David <cruz-azul99@hotmail.com>
2021-02-01 12:46:27 -0600

    2 routes added for redirection

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,13 @@ import InicioComponent from './components/inicioComponent.vue'
 import UsuariosComponent from './components/Usuarios.vue'
 import InfoUsuario from './components/infoUsuario.vue'
 
+// Lazy loading: Sólo lo carga cuando necesitemos el componente, se debe comentar el import de UsuariosComponent
+// const UsuariosComponent = resolve => {
+//     require.ensure(['./components/Usuarios.vue'], () => {
+//         resolve(require('./components/Usuarios.vue'))
+//     })
+// }
+
 // Las rutas son un arreglo de objetos
 export const routes = [
     {
@@ -25,6 +32,16 @@ export const routes = [
         path: '/usuarios/:id',
         component: InfoUsuario,
         name: 'InfoUsuario'
+    },
+    // Forma de redireccionar a otra ruta
+    {
+        path: '/users',
+        redirect: '/usuarios'
+    },
+    // Redireccionar cuando alguien escribe cualquier cosa, es decir, cuando ninguna de las rutas anteriores es llamada, entonces te redirecciona
+    {
+        path: '*',
+        redirect: '/'
     }
     
 ]
