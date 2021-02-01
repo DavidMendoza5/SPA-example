08f1163cc1f7fa8be44c628b3b264690cadb8aa4
David <cruz-azul99@hotmail.com>
2021-02-01 12:32:34 -0600

    Method volver created with a button on the template

--- src/components/infoUsuario.vue
+++ src/components/infoUsuario.vue
@@ -5,6 +5,7 @@
             <p><strong>Nombre: </strong>{{ name }}</p>
             <p><strong>Correo: </strong>{{ email }}</p>
         </div>
+        <button class="btn btn-primary" @click="volver">Volver</button>
     </div>
 </template>
 
@@ -17,13 +18,20 @@ export default {
         }
     },
     // Funciona similar a un computed, pero este se encarga de revisar si hubo algún cambio en la página, en este caso, un cambio en la ruta
-    watch: {
-        $route() {
-            let id = this.$route.params.id
-            this.$http.get('https://jsonplaceholder.typicode.com/users/' + id).then(respuesta => respuesta.json()).then(usuarios => {
-            this.name = usuarios.name
-            this.email = usuarios.email
-            })
+    // watch: {
+    //     $route() {
+    //         let id = this.$route.params.id
+    //         this.$http.get('https://jsonplaceholder.typicode.com/users/' + id).then(respuesta => respuesta.json()).then(usuarios => {
+    //         this.name = usuarios.name
+    //         this.email = usuarios.email
+    //         })
+    //     }
+    // },
+    methods: {
+        volver() {
+            // Este método permite poner una ruta cuando es llamado, igual se le puede poner el nombre de la ruta
+            // {name: 'nombreRuta'}, en lugar de poner la ruta tal cual
+            this.$router.push('/usuarios')
         }
     },
     created() {
