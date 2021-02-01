925595c733ce5a91bc6faa4109da43ef818e0bc6
David <cruz-azul99@hotmail.com>
2021-02-01 12:17:49 -0600

    Method watch added

--- src/components/infoUsuario.vue
+++ src/components/infoUsuario.vue
@@ -16,6 +16,16 @@ export default {
             email: ''
         }
     },
+    // Funciona similar a un computed, pero este se encarga de revisar si hubo algún cambio en la página, en este caso, un cambio en la ruta
+    watch: {
+        $route() {
+            let id = this.$route.params.id
+            this.$http.get('https://jsonplaceholder.typicode.com/users/' + id).then(respuesta => respuesta.json()).then(usuarios => {
+            this.name = usuarios.name
+            this.email = usuarios.email
+            })
+        }
+    },
     created() {
         let id = this.$route.params.id
         this.$http.get('https://jsonplaceholder.typicode.com/users/' + id).then(respuesta => respuesta.json()).then(usuarios => {
