cc2cd1efe5eca7238e8d55ff07608aeb6dc57743
David <cruz-azul99@hotmail.com>
2021-02-01 11:33:20 -0600

    Component created - This component shows a usar based on the id send on params

---
+++ src/components/infoUsuario.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Información del usuario</h3>
+        <div>
+            <p><strong>Nombre: </strong>{{ name }}</p>
+            <p><strong>Correo: </strong>{{ email }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    created() {
+        let id = this.$route.params.id
+        this.$http.get('https://jsonplaceholder.typicode.com/users/' + id).then(respuesta => respuesta.json()).then(usuarios => {
+            this.name = usuarios.name
+            this.email = usuarios.email
+        })
+    }
+}
+</script>
